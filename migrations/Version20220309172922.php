e9603979cd06ea4161bafea218956d3b45ce426b
richeenektar <robin011@live.de>
2022-03-09 22:41:22 +0100

    fix: migration?

--- migrations/Version20220309172922.php
+++ migrations/Version20220309172922.php
@@ -13,12 +13,11 @@ final class Version20220309172922 extends AbstractMigration
     public function up(Schema $schema): void
     {
         foreach([
-            'ADD `user_id` INT DEFAULT 0',
-            'DROP PRIMARY KEY',
-            'ADD PRIMARY KEY (`tmdb_id`, `user_id`)',
+            'ADD `user_id` INT',
+            'DROP PRIMARY KEY, ADD PRIMARY KEY (`tmdb_id`, `user_id`)',
             'ADD CONSTRAINT `request_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE ON DELETE CASCADE',
         ] as $query) {
-            $this->addSql("ALTER TABLE `request` $query");
+            $this->addSql("ALTER TABLE `request` ");
         }
     }
 
@@ -26,9 +25,7 @@ final class Version20220309172922 extends AbstractMigration
     {
         foreach([
             'DROP FOREIGN KEY `request_user_id`',
-            'DROP FOREIGN KEY `request_tmdb_id`',
-            'DROP PRIMARY KEY',
-            'ADD PRIMARY KEY (`tmdb_id`)',
+            'DROP PRIMARY KEY, ADD PRIMARY KEY (`tmdb_id`)',
             'DROP `user_id`',
         ] as $query) {
             $this->addSql("ALTER TABLE `request` $query");
