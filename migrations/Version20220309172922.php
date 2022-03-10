c215844f2c9fc1319755e7d0d5f0bbe2c5c3c305
richeenektar <robin011@live.de>
2022-03-10 20:57:37 +0100

    fix: migrations

--- migrations/Version20220309172922.php
+++ migrations/Version20220309172922.php
@@ -20,9 +20,9 @@ final class Version20220309172922 extends AbstractMigration
         foreach([
             'ADD `user_id` INT',
             'DROP PRIMARY KEY, ADD PRIMARY KEY (`tmdb_id`, `user_id`)',
-            'ADD CONSTRAINT `request_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE ON DELETE CASCADE',
+            'ADD CONSTRAINT `request_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)',
         ] as $query) {
-            $this->addSql("ALTER TABLE `request` ");
+            $this->addSql("ALTER TABLE `request` $query");
         }
     }
 
