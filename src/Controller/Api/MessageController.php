682b491cd9bab795ffc36ed39278de7db12f273c
richeenektar <robin011@live.de>
2022-03-16 00:29:21 +0100

    fix: messages security issue

--- src/Controller/Api/MessageController.php
+++ src/Controller/Api/MessageController.php
@@ -9,25 +9,32 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class MessageController extends AbstractController
 {
     private MessageRepository $messageRepository;
     private EntityManagerInterface $entityManager;
+    private Security $security;
 
     public function __construct(
         MessageRepository $messageRepository,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        Security $security
     ) {
         $this->messageRepository = $messageRepository;
         $this->entityManager = $entityManager;
+        $this->security = $security;
     }
 
     /**
-     * @Route(name="api_list_messages", path="/api/messages/{user<\d+>}/{count<\d+>}", methods="GET")
+     * @Route(name="api_list_messages", path="/api/messages/{count<\d+>}", methods="GET")
      */
-    public function listMessages(User $user, int $count = 0): Response
+    public function listMessages(int $count = 0): Response
     {
+        /** @var User $user */
+        $user = $this->security->getUser();
+
         if ($count > 0) {
             return $this->json($this->messageRepository->findMaxForUser($user, $count));
         }
